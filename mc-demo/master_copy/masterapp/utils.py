1ebe260e9361338fc679fed93de3e91e31daa33e
Dhamu-Lmark <162978775+Dhamu-Lmark@users.noreply.github.com>
2024-04-01 16:41:51 +0530

    Delete mc-demo directory

--- mc-demo/master_copy/masterapp/utils.py
+++
@@ -1,9 +0,0 @@
-# utils.py
-
-import random
-import string
-
-def generate_token(length=32):
-    """Generate a random token."""
-    characters = string.ascii_letters + string.digits
-    return ''.join(random.choice(characters) for _ in range(length))
